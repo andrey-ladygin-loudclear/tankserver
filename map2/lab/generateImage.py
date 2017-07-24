5a1577526bb9c4c5320e4fe526c6b86348e838bb
andrey <andrey@gmail.com>
2017-07-24 15:29:38 +0300

    map2

--- map2/lab/generateImage.py
+++ map2/lab/generateImage.py
@@ -1,8 +1,11 @@
 import json
+import random
 
 from PIL import Image
 
-src = '../assets/177.jpg'
+src1 = '../assets/177.jpg'
+src2 = '../assets/178.jpg'
+src3 = '../assets/179.jpg'
 
 x_blockCount = 70
 y_blockCount = 120
@@ -11,13 +14,16 @@ mw = 32 * x_blockCount
 mh = 32 * y_blockCount
 
 out = Image.new('RGBA', (mw, mh))
-im = Image.open(src)
+img = [
+    Image.open(src1),
+    Image.open(src2),
+]
 
 for i in range(x_blockCount):
     for j in range(y_blockCount):
         x = i * 32
         y = j * 32
-        out.paste(im, (x, y))
+        out.paste(random.choice(img), (x, y))
 
 out.save("image.png")
 out.show()
